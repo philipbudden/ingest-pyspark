f6f3bc7323e798e9e7af916e11d8870fff1c8390
n3ddu8 <13167147+n3ddu8@users.noreply.github.com>
2025-12-01 14:51:37 +0000

    feat: paramaterised and added pre-create

--- main.py
+++ main.py
@@ -1,35 +1,61 @@
 from datetime import datetime, date
 
-from pyspark.sql import SparkSession, Row
 from delta import *
+from pyspark.sql import SparkSession, Row
+from pyspark.sql.functions import *
+import yaml
 
-builder = SparkSession.builder.appName("MyApp") \
-    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+def run(
+    config: dict,
+):
 
-spark = configure_spark_with_delta_pip(builder).getOrCreate()
+    deltapath = config["parameters"]["deltapath"]
 
-spark.createDataFrame([
-    Row(a=1, b=2., c='string1', d=date(2000, 1, 1), e=datetime(2000, 1, 1, 12, 0)),
-    Row(a=2, b=3., c='string2', d=date(2000, 2, 1), e=datetime(2000, 1, 2, 12, 0)),
-    Row(a=4, b=5., c='string3', d=date(2000, 3, 1), e=datetime(2000, 1, 3, 12, 0)),
-]).write.format("delta").save("/tmp/delta-table")
+    builder = (SparkSession.builder.appName("MyApp")
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+    )
 
-df = spark.read.format("delta").load("/tmp/delta-table")
-df.show()
+    spark = configure_spark_with_delta_pip(builder).getOrCreate()
 
-spark.createDataFrame([
-    Row(a=1, b=2., c='string1', d=date(2000, 1, 1), e=datetime(2000, 1, 1, 12, 0)),
-    Row(a=2, b=3., c='string2', d=date(2000, 2, 1), e=datetime(2000, 1, 2, 12, 0)),
-    Row(a=4, b=5., c='string3', d=date(2000, 3, 1), e=datetime(2000, 1, 3, 12, 0)),
-    Row(a=5, b=6., c='string4', d=date(2000, 4, 1), e=datetime(2000, 1, 4, 12, 0)),
-]).write.format("delta").mode("overwrite").save("/tmp/delta-table")
+    if not DeltaTable.isDeltaTable(spark, f"{deltapath}/deltatable"):
+        spark.sql(f"""
+            CREATE TABLE deltatable(
+                   a BIGINT
+                   ,b DOUBLE
+                   ,c STRING
+                   ,d DATE
+                   ,e TIMESTAMP
+            ) USING DELTA
+            LOCATION '{deltapath}'
+        """)
 
-df = spark.read.format("delta").load("/tmp/delta-table")
-df.show()
+    spark.createDataFrame([
+        Row(a=1, b=2., c='string1', d=date(2000, 1, 1), e=datetime(2000, 1, 1, 12, 0)),
+        Row(a=2, b=3., c='string2', d=date(2000, 2, 1), e=datetime(2000, 1, 2, 12, 0)),
+        Row(a=4, b=5., c='string3', d=date(2000, 3, 1), e=datetime(2000, 1, 3, 12, 0)),
+    ]).write.format("delta").mode("overwrite").save(f"{deltapath}/deltatable")
 
-df = spark.read.format("delta") \
-  .option("versionAsOf", 0) \
-  .load("/tmp/delta-table")
+    spark.createDataFrame([
+        Row(a=5, b=6., c='string4', d=date(2000, 4, 1), e=datetime(2000, 1, 4, 12, 0)),
+    ]).write.format("delta").mode("append").save(f"{deltapath}/deltatable")
 
-df.show()
+    deltaTable = DeltaTable.forPath(spark, f"{deltapath}/deltatable")
+
+    df = deltaTable.toDF()
+
+    print(df.dtypes)
+    df.show()
+
+    df = spark.read.format("delta") \
+      .option("versionAsOf", 0) \
+      .load(f"{deltapath}/deltatable")
+
+    df.show()
+
+if __name__ == "__main__":
+
+    with open("config.yaml", "r") as f:
+        config = yaml.safe_load(f)
+
+    run(config)
